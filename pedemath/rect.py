79033d7d646eff2122d6d5819a99014d42d4ef45
Eric Olson <mail@olsoneric.com>
2013-07-05 00:37:13 -0500

    Pep8 fixes

--- pedemath/rect.py
+++ pedemath/rect.py
@@ -7,23 +7,27 @@ class Rect:
         self.height = height
 
     def __str__(self):
-        return str("Rect(%s,%s,%s,%s)" % (self.x, self.y, self.width, self.height) )
+        return str("Rect(%s,%s,%s,%s)" % (
+            self.x, self.y, self.width, self.height))
 
     def __eq__(self, obj):
-        return self.x == obj.x and self.y == obj.y and self.width == obj.width and self.height == obj.height
+        return (self.x == obj.x and self.y == obj.y and self.width == obj.width
+                and self.height == obj.height)
 
     def __repr__(self):
-        return str("Rect(%s,%s,%s,%s)" % (self.x, self.y, self.width, self.height) )
+        return str("Rect(%s,%s,%s,%s)" % (
+            self.x, self.y, self.width, self.height))
 
     def union(self, rect):
         origin = min(self.x, rect.x), min(self.y, rect.y)
-        return Rect(origin[0], origin[1],
-                   max(self.x+self.width, rect.x+rect.width) - origin[0],
-                   max(self.y+self.height, rect.y+rect.height) - origin[1])
+        return Rect(
+            origin[0], origin[1],
+            max(self.x+self.width, rect.x+rect.width) - origin[0],
+            max(self.y+self.height, rect.y+rect.height) - origin[1])
 
     def contains_point(self, x, y):
         if (x >= self.x and x <= self.x + self.width and
-            y >= self.y and y <= self.y + self.height):
+                y >= self.y and y <= self.y + self.height):
             return True
         else:
             return False
@@ -61,15 +65,13 @@ class Rect:
     def bottomright(self):
         return (self.x+self.width, self.y + self.height)
 
-    def colliderect (self, r2):
-        return (((self.x >= r2.x   and self.x < r2.x   + r2.width) or
-                 (r2.x   >= self.x and r2.x   < self.x + self.width)) and
-                ((self.y >= r2.y   and self.y < r2.y   + r2.height)   or
-                 (r2.y   >= self.y and r2.y   < self.y + self.height)))
+    def colliderect(self, r2):
+        return (((self.x >= r2.x and self.x < r2.x + r2.width) or
+                 (r2.x >= self.x and r2.x < self.x + self.width)) and
+                ((self.y >= r2.y and self.y < r2.y + r2.height) or
+                 (r2.y >= self.y and r2.y < self.y + self.height)))
 
     def collidepoint(self, p1):
         x, y = p1[0], p1[1]
         return (x >= self.x and x < self.x + self.width and
                 y >= self.y and y < self.y + self.height)
-
-
