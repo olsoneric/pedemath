fea721cee528db520ad5330275b6b43e12776184
Eric Olson <git@olsoneric.com>
2013-09-08 16:27:14 -0500

    Initial cleanup of old code
    Update to use instance().
    Make pep8 fixes.

--- pedemath/matrix.py
+++ pedemath/matrix.py
@@ -1,156 +1,114 @@
-#from numarray import array, Float32, dot
-try:
-    from numpy import array, dot
-except:
-    try:
-        from Numeric import array, Float32, dot
-        #from numarray import array, Float32, dot
-        print "Numpy import failed, trying to use numarray."
-    except:
-        print "Error Numpy (and Numeric) not available."
 
-from types import InstanceType
 import math
-from pede3.pmath.vec3 import Vec3
 
-class Matrix44f:
+from numpy import array, dot
+
+# TODO: see if array is a good replacement for numpy
+
+from pedemath.vec3 import Vec3
+
+
+class Matrix44(object):
     # C and numarray matrix is in row-major order ( [row][column] )
     # This class will try to return data in column-major order [column][row]
 
     def __init__(self):
-        self.makeIdentity()
+        self.make_identity()
 
-    def makeIdentity(self):
+    def make_identity(self):
         """
-    #cols[0][0] = 1.0f; cols[1][0] = 0.0f; cols[2][0] = 0.0f; cols[3][0] = 0.0f;
-    #cols[0][1] = 0.0f; cols[1][1] = 1.0f; cols[2][1] = 0.0f; cols[3][1] = 0.0f;
-    #cols[0][2] = 0.0f; cols[1][2] = 0.0f; cols[2][2] = 1.0f; cols[3][2] = 0.0f;
-    #cols[0][3] = 0.0f; cols[1][3] = 0.0f; cols[2][3] = 0.0f; cols[3][3] = 1.0f;
+   #cols[0][0] = 1.0f; cols[1][0] = 0.0f; cols[2][0] = 0.0f; cols[3][0] = 0.0f;
+   #cols[0][1] = 0.0f; cols[1][1] = 1.0f; cols[2][1] = 0.0f; cols[3][1] = 0.0f;
+   #cols[0][2] = 0.0f; cols[1][2] = 0.0f; cols[2][2] = 1.0f; cols[3][2] = 0.0f;
+   #cols[0][3] = 0.0f; cols[1][3] = 0.0f; cols[2][3] = 0.0f; cols[3][3] = 1.0f;
         """
 
-        self.data = array( [[1,0,0,0],
-                            [0,1,0,0],
-                            [0,0,1,0],
-                            [0,0,0,1]], dtype="float32")
+        #self.data = array('f', [[1., 0., 0., 0.],
+        #                        [0., 1., 0., 0.],
+        #                        [0., 0., 1., 0.],
+        #                        [0., 0., 0., 1.]])
+        self.data = array([[1, 0, 0, 0],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]], dtype="float32")
 
     def __str__(self):
         return str(self.data)
 
     def __rsub__(self, other):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                self.data = self.data - obj.data
-            else:
-                raise
+        if isinstance(other, Matrix44):
+            self.data = self.data - other.data
         else:
-            raise
+            raise Exception(
+                "Matrix.__rsub__ arg is not a matrix. %s" % type(other))
 
-    def __sub__(self, obj):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                mat = Matrix44f()
-                mat.data = self.data - obj.data
-                return mat
-            else:
-                raise
+    def __sub__(self, other):
+        if isinstance(other, Matrix44):
+            mat = Matrix44()
+            mat.data = self.data - other.data
+            return mat
         else:
-            raise
+            raise Exception(
+                "Matrix.__sub__ arg is not a matrix. %s" % type(other))
 
     def __radd__(self, other):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                self.data = self.data + obj.data
-            else:
-                raise
+        if isinstance(other, Matrix44):
+            self.data = self.data + other.data
         else:
-            raise
+            raise Exception(
+                "Matrix.__sub__ arg is not a matrix. %s" % type(other))
 
-    def __add__(self, obj):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                mat = Matrix44f()
-                mat.data = self.data + obj.data
-                return mat
-            else:
-                raise
+    def __add__(self, other):
+        if isinstance(other, Matrix44):
+            mat = Matrix44()
+            mat.data = self.data + other.data
+            return mat
         else:
-            raise
+            raise Exception(
+                "Matrix.__add__ arg is not a matrix. %s" % type(other))
 
-
-    def __rmul__(self, obj):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                self.data = dot(self.data, obj.data)
-            else:
-                raise
+    def __rmul__(self, other):
+        if isinstance(other, Matrix44):
+            self.data = dot(self.data, other.data)
         else:
-            raise Exception("Multiplying: %s %s", self.__class__.__name__, type(obj))
+            raise Exception(
+                "Matrix44.__rmul__, arg is not a matrix: %s" % type(other))
 
-    def __mul__(self, obj):
-        if type(obj) == Matrix44f:
-                mat = Matrix44f()
-                mat.data = dot(self.data , obj.data)
-                return mat
-        elif type(obj) == Vec3:
-                v = obj
-                # just copied from above, delete above checks 
-                return Vec3(v[0]*self.data[0][0] + v[1] * self.data[1][0] + v[2] * self.data[2][0] + self.data[3][0],
-                  v[0] * self.data[0][1] + v[1] * self.data[1][1] + v[2] * self.data[2][1] + self.data[3][1],
-                   v[0] * self.data[0][2] + v[1] * self.data[1][2] + v[2] * self.data[2][2] + self.data[3][2])
-        # older python checks for InstanceType
-        elif type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                mat = Matrix44f()
-                mat.data = dot(self.data , obj.data)
-                return mat
-            elif obj.__class__ == Vec3:
-                v = obj
-                return Vec3(v[0]*self.data[0][0] + v[1] * self.data[1][0] + v[2] * self.data[2][0] + self.data[3][0],
-                  v[0] * self.data[0][1] + v[1] * self.data[1][1] + v[2] * self.data[2][1] + self.data[3][1],
-                   v[0] * self.data[0][2] + v[1] * self.data[1][2] + v[2] * self.data[2][2] + self.data[3][2])
-            else:
-                raise Exception("Multiplying: %s %s", self.__class__.__name__, obj.__class__.__name__)
+    def __mul__(self, other):
+        if isinstance(other, Matrix44):
+            mat = Matrix44()
+            mat.data = dot(self.data, other.data)
+            return mat
+        elif isinstance(other, Vec3):
+            v = other
+            # just copied from above, delete above checks
+            return Vec3(v[0]*self.data[0][0] + v[1] * self.data[1][0] +
+                        v[2] * self.data[2][0] + self.data[3][0],
+                        v[0] * self.data[0][1] + v[1] * self.data[1][1] +
+                        v[2] * self.data[2][1] + self.data[3][1],
+                        v[0] * self.data[0][2] + v[1] * self.data[1][2] +
+                        v[2] * self.data[2][2] + self.data[3][2])
         else:
-            raise Exception("Matrix44f, unhandled type %s" % type(obj))
+            raise Exception("Matrix44.__mul__ unhandled type %s" % type(other))
 
-    """
-    def __rdiv__(self, other):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                self.data = self.data / obj.data
-            else:
-                raise
-        else:
-            raise
 
-    def __div__(self, obj):
-        if type(obj) == InstanceType:
-            if obj.__class__ == self.__class__:
-                mat = Matrix44f()
-                mat.data = self.data / obj.data
-                return mat
-            else:
-                raise
-        else:
-            raise
-    """
-
-def Matrix44fTrans(transVec):
-    mat = Matrix44f()
+def matrix44_trans(trans_vec):
+    mat = Matrix44()
     # internal data is row major order
-    mat.data[3][0] = transVec[0]
-    mat.data[3][1] = transVec[1]
-    mat.data[3][2] = transVec[2]
-    return  mat
+    mat.data[3][0] = trans_vec[0]
+    mat.data[3][1] = trans_vec[1]
+    mat.data[3][2] = trans_vec[2]
+    return mat
 
-def Matrix44fRotY(angleDegrees):
+
+def matrix44_rot_y(angle_degrees):
     # X^2 * (1-c) + c  |  xy(1-c) -zs     | xz(1-c) +ys     | 0
     # yx(1-c) + zs     |  y^2 * (1-c) +c  | yz(1-c) -xs     | 0
     # xz(1-c) - ys     |  yz(1-c) +xs     | z^2 * (1-c) + z | 0
     # 0                |  0               | 0               | 1
-    mat = Matrix44f()
-    c = math.cos(angleDegrees * math.pi / 180.)
-    s = math.sin(angleDegrees * math.pi / 180.)
+    mat = Matrix44()
+    c = math.cos(angle_degrees * math.pi / 180.)
+    s = math.sin(angle_degrees * math.pi / 180.)
     #       row, column for internal storage
     mat.data[0][0] = c
     mat.data[2][0] = s
@@ -158,10 +116,11 @@ def Matrix44fRotY(angleDegrees):
     mat.data[2][2] = c
     return mat
 
-def Matrix44fRotX(angleDegrees):
-    mat = Matrix44f()
-    c = math.cos(angleDegrees * math.pi / 180.)
-    s = math.sin(angleDegrees * math.pi / 180.)
+
+def matrix44_rot_x(angle_degrees):
+    mat = Matrix44()
+    c = math.cos(angle_degrees * math.pi / 180.)
+    s = math.sin(angle_degrees * math.pi / 180.)
     #       row, column for internal storage
     mat.data[1][1] = c
     mat.data[2][1] = -s
@@ -169,10 +128,11 @@ def Matrix44fRotX(angleDegrees):
     mat.data[2][2] = c
     return mat
 
-def Matrix44fRotZ(angleDegrees):
-    mat = Matrix44f()
-    c = math.cos(angleDegrees * math.pi / 180.)
-    s = math.sin(angleDegrees * math.pi / 180.)
+
+def matrix44_rot_z(angle_degrees):
+    mat = Matrix44()
+    c = math.cos(angle_degrees * math.pi / 180.)
+    s = math.sin(angle_degrees * math.pi / 180.)
     #       row, column for internal storage
     mat.data[0][0] = c
     mat.data[1][0] = -s
@@ -180,27 +140,26 @@ def Matrix44fRotZ(angleDegrees):
     mat.data[1][1] = c
     return mat
 
-def rotateV3fDegXYZ(vecA, rot):
-    rotMat = Matrix44fRotX(rot[0])
-    newVec = rotMat * vecA
 
-    rotMat = Matrix44fRotY(rot[1])
-    newVec = rotMat * newVec
+def rotate_v3f_deg_xyz(vec_a, rot):
+    rot_mat = matrix44_rot_x(rot[0])
+    new_vec = rot_mat * vec_a
 
-    rotMat = Matrix44fRotZ(rot[2])
-    newVec = rotMat * newVec
-    return newVec
+    rotMat = matrix44_rot_y(rot[1])
+    new_vec = rotMat * new_vec
 
-
+    rot_mat = matrix44_rot_z(rot[2])
+    new_vec = rot_mat * new_vec
+    return new_vec
 
 
 if __name__ == "__main__":
     print "Identity:"
-    m = Matrix44f()
+    m = Matrix44()
     print m.data
 
     print "Add:"
-    m2 = Matrix44f()
+    m2 = Matrix44()
     m2.data[0][0] = 5
     m2.data[0][1] = 5
     m2.data[0][2] = 5
@@ -213,45 +172,46 @@ if __name__ == "__main__":
     print "Multiply:"
     print (m * m2).data
 
-    #print "Divide:"
-    #m2 = Matrix44f()
-    #print (m / m2).data
-
-
-    print "Matrix44fTrans:"
-    m3 = Matrix44fTrans(Vec3(2,3,4))
+    print "matrix44_trans:"
+    m3 = matrix44_trans(Vec3(2, 3, 4))
     print m3.data
 
-    print "Matrix44fRotY:"
-    m4 = Matrix44fRotY(45)
+    print "matrix44_rot_y:"
+    m4 = matrix44_rot_y(45)
     print m4.data
 
-   
     print "unit x vec rotated by 45:"
-    v = Vec3(1,0,0)
+    v = Vec3(1, 0, 0)
     print m4 * v
 
     print "unit x vec rotated by 180:"
-    print Matrix44fRotY(180) * v
+    print matrix44_rot_y(180) * v
 
     print "unit x vec translateed by -5,6,-7:"
-    print Matrix44fTrans(Vec3(-5,6,-7)) * v
+    print matrix44_trans(Vec3(-5, 6, -7)) * v
 
     print "translated and rotated origin:"
-    print Matrix44fRotY(45) * Matrix44fTrans(Vec3(1,0,0)) * Vec3(0,0,0)
-    print "a:",Matrix44fTrans(Vec3(1,0,0)) * Vec3(0,0,0)
-    print "b:",Matrix44fRotY(45) * (Matrix44fTrans(Vec3(1,0,0)) * Vec3(0,0,0))
-    c=Matrix44fRotY(45) * Matrix44fTrans(Vec3(1,0,0))
-    print "c:",c
-    print "c.5:",c*Vec3(0,0,0)
-    print "d:",Matrix44fRotY(45) * Matrix44fTrans(Vec3(1,0,0)) * Vec3(1,0,0)
-    print "e:",Matrix44fTrans(Vec3(1,0,0)) * Matrix44fTrans(Vec3(1,0,0)) * Matrix44fTrans(Vec3(0,1,0)) * Matrix44fTrans(Vec3(0,0,1))
-    print "f:",Matrix44fRotY(45) * Vec3(1,0,0)
-    print "right:", (Matrix44fRotY(45) * Matrix44fTrans(Vec3(1,0,0))) * Vec3(0,0,0)
-    print "right 2 (r t i):", (Matrix44fRotY(45) * Matrix44fTrans(Vec3(1,0,0))) * Vec3(0,0,1)
-    print "right 3 (t r i):", (Matrix44fTrans(Vec3(1,0,0)) * Matrix44fRotY(45) ) * Vec3(0,0,1)
-    print "      3b (t r i):", (Matrix44fTrans(Vec3(1,0,0)) * Matrix44fRotY(45) ) * Vec3(0,0,1)
-    print "correct 4 (t r i):", (Matrix44fTrans(Vec3(1,0,0)) * Matrix44fRotY(90) ) * Vec3(0,0,1)
-    print "        4a (t r i):", Matrix44fRotY(90)  * Vec3(0,0,1)
-
-
+    print matrix44_rot_y(45) * matrix44_trans(Vec3(1, 0, 0)) * Vec3(0, 0, 0)
+    print "a:", matrix44_trans(Vec3(1, 0, 0)) * Vec3(0, 0, 0)
+    print "b:", matrix44_rot_y(45) * (
+        matrix44_trans(Vec3(1, 0, 0)) * Vec3(0, 0, 0))
+    c = matrix44_rot_y(45) * matrix44_trans(Vec3(1, 0, 0))
+    print "c:", c
+    print "c.5:", c*Vec3(0, 0, 0)
+    print "d:", matrix44_rot_y(45) * matrix44_trans(
+        Vec3(1, 0, 0)) * Vec3(1, 0, 0)
+    print "e:", matrix44_trans(Vec3(1, 0, 0)) * matrix44_trans(
+        Vec3(1, 0, 0)) * matrix44_trans(Vec3(0, 1, 0)) * matrix44_trans(
+            Vec3(0, 0, 1))
+    print "f:", matrix44_rot_y(45) * Vec3(1, 0, 0)
+    print "right:", (matrix44_rot_y(45) * matrix44_trans(
+        Vec3(1, 0, 0))) * Vec3(0, 0, 0)
+    print "right 2 (r t i):", (matrix44_rot_y(45) * matrix44_trans(
+        Vec3(1, 0, 0))) * Vec3(0, 0, 1)
+    print "right 3 (t r i):", (matrix44_trans(
+        Vec3(1, 0, 0)) * matrix44_rot_y(45)) * Vec3(0, 0, 1)
+    print "      3b (t r i):", (matrix44_trans(
+        Vec3(1, 0, 0)) * matrix44_rot_y(45)) * Vec3(0, 0, 1)
+    print "correct 4 (t r i):", (matrix44_trans(
+        Vec3(1, 0, 0)) * matrix44_rot_y(90)) * Vec3(0, 0, 1)
+    print "        4a (t r i):", matrix44_rot_y(90) * Vec3(0, 0, 1)
