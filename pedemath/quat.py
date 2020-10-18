4bfee33efb27e2d4761d680749aac1b1dae1eaab
Eric Olson <git@olsoneric.com>
2020-10-18 01:10:50 -0500

    Add from_axis_angle_rad and from_axis_angle_deg

--- pedemath/quat.py
+++ pedemath/quat.py
@@ -233,14 +233,34 @@ class Quat(object):
         return math.asin(-2.0 * (self.x * self.z - self.y * self.w))
 
     def get_y_rot_deg(self):
+        """From a this Quat rotation, extract the rotation around the y axis.
+
+        Mainly converts get_y_rot_rads() result to degrees.
+        """
         return self.get_y_rot_rads() * 180.0 / math.pi
 
     @staticmethod
-    def from_axis_angle(axis_v3, angle_deg):
+    def from_axis_angle_deg(axis_v3, angle_deg):
+        """Return a new Quat for a rotation around a vector.
+
+        Angle is expected in degrees.
+        """
 
         axis_v3 = normalize_v3(axis_v3)
 
         angle_rad = angle_deg * math.pi / 180.
+
+        return Quat.from_axis_angle_rad(axis_v3, angle_rad)
+    
+    # Temporary for backwards compatibility
+    from_axis_angle = from_axis_angle_deg
+
+    @staticmethod
+    def from_axis_angle_rad(axis_v3, angle_rad):
+        """Return a new Quat for a rotation around a vector.
+
+        Angle is expected in radians.
+        """
 
         quat = Quat(math.sin(angle_rad / 2.0) * axis_v3[0],
                     math.sin(angle_rad / 2.0) * axis_v3[1],
